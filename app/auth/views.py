dc99e912cde0fd022bc3bbe346964d823571a2a0
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-16 09:00:49 +0300

    Add login function in views.py.

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,3 +4,14 @@ from flask_login import login_user,login_required,logout_user
 from .forms import RegForm,LoginForm
 from ..models import User
 from .. import db
+
+@auth.route('/login', methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user != None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+    return render_template('auth/login.html', loginform = form)
