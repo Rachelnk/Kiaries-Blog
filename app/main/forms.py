3b7e6362b270075d5eee0cad8390b14b01d4991f
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-16 15:21:16 +0300

    Import email validator.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,19 @@
 from . import main
 from flask_wtf import FlaskForm
 from wtforms import SubmitField,StringField,TextAreaField,SelectField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired,Email,EqualTo, ValidationError
 from ..models import Blog_Post, Comment
 
 class PostForm(FlaskForm):
   title = StringField ('Title', validators= [InputRequired()])
   content = TextAreaField('Content',validators=[InputRequired()])
   submit = SubmitField('Add Pitch')
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Comment on the post',validators=[InputRequired()])
+    submit = SubmitField('Add Comment')
+class SubscriberForm(FlaskForm):
+    email = StringField('Enter Your Email Address', validators=[InputRequired(),Email()])
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write a bit about yourself',validators=[InputRequired()])
+    submit = SubmitField('Add Bio')
