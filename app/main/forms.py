607425827a2e70f127d3f25593add25351ca8644
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-16 10:37:16 +0300

    Create blog post form to create a new blog post.

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,4 +2,9 @@ from . import main
 from flask_wtf import FlaskForm
 from wtforms import SubmitField,StringField,TextAreaField,SelectField
 from wtforms.validators import InputRequired
-from ..models import Pitch,Comment
+from ..models import Blog_Post, Comment
+
+class PostForm(FlaskForm):
+  title = StringField ('Title', validators= [InputRequired()])
+  content = TextAreaField('Content',validators=[InputRequired()])
+  submit = SubmitField('Add Pitch')
