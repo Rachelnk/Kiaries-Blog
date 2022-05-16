eb0b85dd2ab535bc7c6320b7a88b541aa8762bc7
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-16 07:30:19 +0300

    Import login manager, uploadset, configure_photos extensions.

--- app/models.py
+++ app/models.py
@@ -5,12 +5,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 class User (db.Model, UserMixin):
   __tablename__ = 'users'
-  id = db.Column(db.Interger, primary_key =True)
+  id = db.Column(db.Integer, primary_key =True)
   username = db.Column (db.String(255), unique = True, nullable = True)
   email = db.Column (db.String(255), unique = True, nullable = True)
   bio = db.Column (db.String(255),nullable = True)
   profile_pic = db.Column (db.String(255),nullable = True)
-  pass_secure = db.column(db.string(255))
+  pass_secure = db.column(db.String(255))
 
 
   @property
@@ -32,14 +32,14 @@ class User (db.Model, UserMixin):
   def __repr__(self):
       return f'User{self.username}'
 
-class Blog_Post(db.model):
+class Blog_Post(db.Model):
       '''
       model that contains database schema for blog post table
       '''
       __tablename__ = 'posts'
       id = db.Column(db.Integer, primary_key = True)
       tile = db.Column(db.String(255), nullable = False)
-      content = db.Colum(db.Text(), nullable = False)
+      content = db.Column(db.Text(), nullable = False)
       time_posted = db.Column (db.DateTime, default=datetime.utcnow)
       user_id=db.Column(db.Integer, db.ForeignKey("users.id"))
       comment = db.relationship('Comment' , backref = 'blog_post', lazy = 'dynamic')
