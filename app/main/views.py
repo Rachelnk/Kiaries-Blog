acbd532b8470e5b03b15552c757721efded4752f
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-16 16:50:20 +0300

    Add addpost html file and afunction to add new posts in views.py file.

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,22 @@ from flask_login import login_required,current_user,login_user,logout_user
 from .forms import PostForm,CommentsForm, UpdateProfile
 from ..models import User, Comment, Blog_Post
 from .. import db, photos
+
+@main.route('/')
+def index():
+    all_posts = Blog_Post.query.all()
+    title = 'Home -  Welcome to Kiarie\'s Blog'
+    return render_template('index.html')
+
+@main.route('/create_new', methods = ['POST','GET'])
+@login_required
+def new_pitch():
+    form = PostForm
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.post.data
+        user_id = current_user
+        new_post_object = Blog_Post(content = content ,user_id=current_user._get_current_object().id,title=title)
+        new_post_object.save_post()
+        return redirect(url_for('main.index'))
+    return render_template('add_post.html', form = form)
