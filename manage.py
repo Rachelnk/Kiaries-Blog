8d5c6de7d02e0441342f88010ed16fd48ca2195b
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-18 20:48:47 +0300

    Add test configurations in config.py.

--- manage.py
+++ manage.py
@@ -8,6 +8,12 @@ manager = Manager(app)
 manager.add_command('server', Server)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
