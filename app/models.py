f33c0e9333f20f9edbc5a96ebde2df7fe10a5610
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-18 21:16:15 +0300

    Change name of subscribers' table,

--- app/models.py
+++ app/models.py
@@ -91,7 +91,7 @@ class Comment(db.Model):
       return f'Comment {self.comment}'
 
 class Subscriber(db.Model):
-    __tablename__='subscribers'
+    __tablename__='subscriberss'
 
     id=db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String(255),unique=True,index=True)
